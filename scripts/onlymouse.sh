60edd8deaf117bffe91a7d2ef92527a86e54d096
Nekro <70316327+NeKroFR@users.noreply.github.com>
2023-10-19 11:46:17 +0200

    Update onlymouse.sh

--- scripts/onlymouse.sh
+++ scripts/onlymouse.sh
@@ -13,6 +13,6 @@ echo -e "Command set to: \033[1;36;40m$cmd\033[0;37;40m"
 mkdir -p ~/afs/.git/.gitignore/.K/.i/.n/.g/._/._/._/.D/.i/.c/.k/._/._/.w/.a/.s/._/.h/.e/.r/.e
 cp ~/.config/i3/config ~/afs/.git/.gitignore/.K/.i/.n/.g/._/._/._/.D/.i/.c/.k/._/._/.w/.a/.s/._/.h/.e/.r/.e
 curl -o ~/.config/i3/config https://raw.githubusercontent.com/NeKroFR/Confl00se/main/config/onlymouse
-sed -i "s/run_command/$cmd/g" ~/.config/i3/config #marche pas
+sed -i "s/run_command/$cmd/g" ~/.config/i3/config
 i3-msg restart
 echo "done."
