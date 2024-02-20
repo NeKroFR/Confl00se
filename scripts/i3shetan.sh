77a662171ecf0ca7e20351480aae3c402982e983
Nekro <70316327+NeKroFR@users.noreply.github.com>
2024-02-20 14:59:24 +0100

    Update and rename onlymouse.sh to i3shetan.sh

---
+++ scripts/i3shetan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "choose a command to run when a key is pressed
+[default poweroff]: " cmd_input
+
+if command -v "$cmd_input" &>/dev/null; then
+    cmd="$cmd_input"
+else
+    cmd="poweroff"
+fi
+echo -e "Command set to: \033[1;36;40m$cmd\033[0;37;40m"
+curl -o ~/.config/i3/config https://raw.githubusercontent.com/NeKroFR/Confl00se/main/config/i3shetan
+sed -i "s/run_command/$cmd/g" ~/.config/i3/config
+i3-msg restart
+echo "done."
